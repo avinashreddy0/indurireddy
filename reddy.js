77f5e23b8c04a738267acfbe7cd32df3df9a5002
Induri Avinash Reddy <induriavinashreddy907@gmail.com>
2024-10-25 14:55:35 +0530

    Add files via upload

---
+++ reddy.js
@@ -0,0 +1,94 @@
+console.log("Hello world");
+console.log('Avinash Reddy');
+console.log('chalapathi college near guntur lam ')
+
+console.log(2 + 2);
+console.log(2 - 3);
+console.log(3 * 4);
+console.log(2 ^ 4);
+console.log(10 / 2);
+console.log(2 + 2+ 2/3 *3 -4^3)
+
+java="avinash"
+console.log(java)
+
+let a=2+2
+console.log(a)
+
+a= 4==5
+console.log(a)
+
+a=5>4
+console.log(a)
+
+let radius=109
+let pi=28.1
+let area
+area= pi*radius*radius
+console.log(area)
+
+let length =2
+let width = 3
+r=length * width
+console.log(r)
+
+let b
+b=a<b&&b>a
+console.log(b)
+
+let t
+t=a||b
+console.log(t)
+
+
+x= (1,2,3,4,5)
+console.log(x)
+
+j = [1,35,47,58]
+console.log(j)
+
+l=[1,2,3,4,5,6,7,8,9,0,0,9,8,7,6,5,4,2]
+console.log(l)
+console.log(l[2])
+console.log(l[7])
+console.log(l[-6])
+
+
+g="he is   good boy"
+console.log(g) 
+
+d= 10 + "volot"
+console.log(d)
+  
+  
+break;
+    }
+}
+    
+    
+    
+    let firstname="avinashreddy"
+  let lastname="i"
+  console.log(firstname,lastname)
+  
+  
+ co=["avinash redey","friend" , "school" , "college", "b-tech"]
+  console.log(co)
+  let f=10
+if (f<12) 
+    console.log(f)
+    
+let list = [1,2,3,4,5,6,7,8]
+
+for (let i=0; i < list.length;i++) {
+    if(list[i]  === 3) {
+        continue;
+    }
+   let j = 50;
+    const k=100;
+    console.log(list[i]);
+    console.log(j);
+    console.log(k);
+    
+    if (list[i] > 5) {
+           
